dea792d0d4c60e662647531a11865527d236da6e
LokeshSaipureddi <66957108+LokeshSaipureddi@users.noreply.github.com>
2020-12-06 20:10:19 +0530

    Create findingmaxandmin.py

---
+++ day1/findingmaxandmin.py
@@ -0,0 +1,9 @@
+a=[]
+n=int(input('Enter the size of array'))
+for i in range(n):
+    b=int(input())
+    a.append(b)
+print('Maximum Element is')    
+print(max(a))
+print('Minimum Element is')
+print(min(a))
